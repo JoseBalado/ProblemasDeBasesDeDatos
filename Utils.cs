9a96191a51d417a1d838317b9f158b3ea5947d22
Jose Balado <JoseBalado@users.noreply.github.com>
2022-03-29 17:09:36 +0200

    Add try-catch block to Utils.FormatedPrint();

--- Utils.cs
+++ Utils.cs
@@ -4,25 +4,34 @@ namespace utils
     {
         public static void FormatedPrint(IEnumerable<object> list)
         {
-            foreach (var property in list.First().GetType().GetProperties())
+            try
             {
-                Console.Write($"| {property.Name,-10}");
-            }
-
-            Console.WriteLine("|");
-
-            foreach (var element in list)
-            {
-                foreach (var property in element.GetType().GetProperties())
+                foreach (var property in list.First().GetType().GetProperties())
                 {
-                    Console.Write($"| {property.GetValue(element),-10}");
+                    Console.Write($"| {property.Name,-10}");
                 }
 
                 Console.WriteLine("|");
+
+                foreach (var element in list)
+                {
+                    foreach (var property in element.GetType().GetProperties())
+                    {
+                        Console.Write($"| {property.GetValue(element),-10}");
+                    }
+
+                    Console.WriteLine("|");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine();
             }
 
-            Console.WriteLine();
-            Console.WriteLine();
+            catch(Exception ex)
+            {
+                Console.WriteLine($"An exception was thrown. Message was: {ex.Message}");
+                Console.WriteLine();
+            }
         }
     }
 }
